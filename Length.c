313f9f84236fe638108fefbc15de15039615ad5c
v.bochechko <vally.1209@gmail.com>
2016-03-19 16:20:33 +0300

    first files

---
+++ Length.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+void main (void)
+{
+	int tonya, ya;
+	char myKey [1000] = "AAAAB3NzaC1yc2EAAAADAQABAAABAQC7xvQys1xRqxFlkl+bCFjVGIhSmOvUz0OtFw4CtlS0rv2BRzYYAOPPSkRT6VzS4B5Jf5dY6kKz7ZpLDU7xBFTgvHIkOkCOXzPS9RjBktboeZepYDU0o7re5oM3dijwAQuXJdwNt0PyczGRa1e9ewKVgo1nZA8YumelPWxICS+xyZ09H16E6OVTNFJwxEYApn4I9dT/o4txwXmMo2fREyd1ZC2eVZbP4BTQn9osY7SEXT8kN1obXd5o3ThtGgiwGaju4hlU2DIu5Jdx9ELFCw2JPBsO+zpJ8a2LtSDkomjMo0t6v8C5l+NCjoCrqhOkVdg+hiCahLzwMy+ktn83XLk9";
+	char tonyKey [1000] = "AAAAB3NzaC1yc2EAAAADAQABAAABAQC6akzDt8mMDweQXwVmEzaF4QJmC3heZaQA9yLjACKHneq92XqGmhiYHV5R6R7bgiyZeq6OKTsw4IyJ9lMGlg9GqDB05VcqdB5YS5vmej3XYaAw1z511VaZQN2twg/s3NIiZAk+m6QvYuGU1K1gdi5d8p5gkUcZc/Grt0/0sgY3041P1cLXnPZIagOqPOmLeeUIFxmagzAOp7dy/5gOxUiV3zx5HGzcgVJdvBZH5JqUvGaYaKzBVa2SGWYs+gm/WFe+qKMk+Sv5fPIE3ApBrDeZxJKH5LLIkErp20vijT0Im7a36uRK2PMUc2/VxFDDjYVOkKlFFbE9y3tksdyttzyH";
+
+	printf("my: %d,  tonya: %d", strlen(myKey), strlen(tonyKey));
+}
