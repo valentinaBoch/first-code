df949b350d9b1fd98ae65bff85d56882b2341e06
v.bochechko <vally.1209@gmail.com>
2016-05-15 14:42:16 +0300

    yandex

--- FileMinMax.c
+++ FileMinMax.c
@@ -1,40 +1,71 @@
 #include <stdio.h>
+int ReadFromFieDayNumber ();///int numDay);
+void WriteToFieDayNumber (int MaxDiff);
+int MaximumDiff(int numDay);
+
 main()
 {
+	int DayNumber, MaxDiff;
+
+	DayNumber = ReadFromFieDayNumber();	//читаем из файла за сколько дней 
+	MaxDiff = MaximumDiff(DayNumber); 	//заполняем массив и находим максимальну разницу значений
+	WriteToFieDayNumber(MaxDiff);           //пишем результат в файл
+}
+
+int ReadFromFieDayNumber ()
+{
+	int numDay;
 	FILE *InputFile;
-	FILE *OutputFile;
-	int N;
-	int m[N];;
-	char str[80];	
-	int i;
-	int min, max, maxN;
-	
 	InputFile = fopen("input.txt","r");
-	OutputFile = fopen("ouput.txt","w");
-
-	fscanf(InputFile, "%s", str);
-        N=atoi(str);
-        printf("N=%d\n",N);
-
-	for (i=0; i<N; i++)
+	if (InputFile == NULL) 
         {
-                fscanf(InputFile, "%s", str);
-                m[i]=atoi(str);
-                printf("%d\n", m[i]);
+                printf ("ERROR! File \"input.txt\" not found.\n");
+                exit(0);
         }
 
-	min = m[0];
-	max = m[0];
-
-	for (i=0; i<N; i++)
-	{
-		if (min > m[i]) min = m[i];
-		if (max < m[i]) max = m[i]; 
-	}
-	printf("min = %d, max = %d\n", min, max);
-	maxN = max-min;
-        fprintf(OutputFile, "max diff =%d",   maxN);
-
+	fscanf(InputFile, "%d", &numDay);
+        printf("DayNumber=%d\n", numDay);
 	fclose(InputFile);
+	return numDay;
+}
+
+
+
+int MaximumDiff(int numDay)
+{
+	int min, max, maxDiff=0;
+	int kurs[numDay];
+	int i = 0;
+	int ch;
+	FILE *InputFile;
+	InputFile = fopen("input.txt","r");
+	
+	while (ch = fgetc(InputFile) != '\n')
+		while (i<numDay)
+		{
+			fscanf(InputFile, "%d", &kurs[i]);
+			printf("kurs[%d]=%d\n", i, kurs[i]);
+			i++;
+		}
+
+       	fclose(InputFile);
+	min = max = kurs[0];
+	for (i=0; i<numDay; i++)
+	{
+		if (min > kurs[i]) min = kurs[i];
+       	        if (max < kurs[i]) max = kurs[i];
+	}
+
+	maxDiff = max - min;
+	printf("maxDiff = %d; numDay = %d\n", maxDiff, numDay);
+
+	return maxDiff;
+}
+
+void WriteToFieDayNumber (int MaxDiff)
+{
+	FILE *OutputFile;
+	OutputFile = fopen("ouput.txt","w");
+	fprintf(OutputFile, "%d\n", MaxDiff);
 	fclose(OutputFile);
 }
